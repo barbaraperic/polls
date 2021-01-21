06af9996e3e0733a4172a91050322b8c0e06824a
Barbara Peric <b.peric10@gmail.com>
2021-01-21 18:19:39 +0100

    FEAT: added stying and html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import App from './components/App'
+import './index.css'
+
+function ColorfulBorder() {
+  return (
+    <React.Fragment>
+      <ul className='border-container'>
+        <li className='border-item' style={{ background: 'var(--red)' }} />
+        <li className='border-item' style={{ background: 'var(--blue)' }} />
+        <li className='border-item' style={{ background: 'var(--pink)' }} />
+        <li className='border-item' style={{ background: 'var(--yellow)' }} />
+        <li className='border-item' style={{ background: 'var(--aqua)' }} />
+      </ul>
+    </React.Fragment>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
+    <ColorfulBorder />
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
-);
-
+)
